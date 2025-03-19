c46fac5cf5e851974f1e550dc9e226b4f32d86bd
Rob <157063279+Rob-Keys@users.noreply.github.com>
2025-03-18 21:28:21 -0400

    Two screens, Bronify and Current Song (My Sunshine)

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,14 +1,13 @@
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
+import CurrentSong from '@/components/CurrentSong';
 import { Text, View } from '@/components/Themed';
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+        <Text style={styles.title}>CURRENT SONG</Text>
+        <CurrentSong path="app/(tabs)/two.tsx" />
     </View>
   );
 }
